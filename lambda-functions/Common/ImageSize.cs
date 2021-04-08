26faf02559d575f9211c98a86fe13cfcdb52d1af
sagulati <sagulati@amazon.com>
2021-04-08 12:41:50 -0500

    Fist pass: adding common objects for state machine.

---
+++ lambda-functions/Common/ImageSize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class ImageSize
+    {
+        /// <summary>
+        /// S3 key
+        /// </summary>
+        public string Key { get; set; }
+
+        public decimal Width { get; set; }
+
+        public decimal Height { get; set; }
+
+        public decimal Size { get; set; }
+    }
+}
---
+++ lambda-functions/Common/State.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Common
+{
+    public class State : ExecutionInput
+    {
+        public long Id { get; set; }
+
+        public string ImageFormat { get; set; }
+
+        public ImageSize FullSize { get; set; }
+
+        public ImageSize ThumbnailSize { get; set; }
+
+        public string ProcessingStatus { get; set; }
+
+        /// <summary>
+        /// Objects detected by AWS Rekognition API
+        /// </summary>
+        public string[] ObjectDetected { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+    }
+}
